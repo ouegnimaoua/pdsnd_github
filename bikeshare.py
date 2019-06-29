32fa352d804ccf8e6b0f77535a813c5a9c1c8c9f
OUEGNIMAOUA Ata <ouegnimaoua@yahoo.fr>
2019-06-29 18:58:03 +0000

    Redefine the constant CITY_DATA

--- bikeshare.py
+++ bikeshare.py
@@ -2,9 +2,12 @@ import time
 import pandas as pd
 import numpy as np
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+#loading data files
+CITY_DATA = {
+    'chicago': 'chicago.csv',
+    'new york city': 'new_york_city.csv',
+    'washington': 'washington.csv'
+}
 
 print("Welcome to the US Bikeshire Data exploration...!\n")
 
